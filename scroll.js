536cd9af7c30e339a6241fe45bdbb131c2299d37
Navaneeth Venu <69888461+nauaneeth@users.noreply.github.com>
2021-07-29 14:14:25 +0530

    Add Scroll Colour Change, Rough Project Main

---
+++ scroll.js
@@ -0,0 +1,31 @@
+window.onscroll = function () { featureslideFunction(); };
+
+function featureslideFunction() {
+    n = 2;
+
+    // for (var i = 0; i < n; ++i) {
+    //     individualElement("panel" + (i + 1).toString())
+    // }
+    individualElement("panel2");
+    // individualElement2("panel1");
+
+}
+
+function individualElement(name) {
+    var slidelement;
+    var bounding, height, width;
+
+    slidelement = document.getElementById(name);
+    height = slidelement.offsetHeight;
+    width = slidelement.offsetWidth;
+    bounding = slidelement.getBoundingClientRect();
+
+    if (bounding.top >= -height && bounding.left >= -width && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + width && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + height) {
+        // Document.body.classList.add("bag-black");
+        document.body.style.backgroundColor = "#121212";
+    }
+    else {
+        document.body.style.backgroundColor = "#5449E8";
+    }
+
+}
