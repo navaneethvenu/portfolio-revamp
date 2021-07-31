30cac0adc0edc7649aae73f6379dd6da57591df1
Navaneeth Venu <69888461+nauaneeth@users.noreply.github.com>
2021-07-31 13:07:36 +0530

    Update Cursor Size

--- scroll.js
+++ scroll.js
@@ -2,6 +2,8 @@ window.onscroll = function () { scrollFunction(); featureslideFunction(); aboutS
 
 function scrollFunction() {
     mybutton = document.getElementById("logo");
+    cursor1 = document.getElementsByClassName("cursorImg")[0];
+    cursor2 = document.getElementsByClassName("cursorImg")[1];
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.opacity = "0.0";
         mybutton.style.transitionDuration = "0.5s";
@@ -9,6 +11,25 @@ function scrollFunction() {
     } else {
         mybutton.style.opacity = "1.0";
     }
+    if ((document.body.scrollTop > 500 && document.body.scrollTop < 1450) || (document.documentElement.scrollTop > 500 && document.documentElement.scrollTop < 1450)) {
+        cursor1.style.width = "300px";
+        cursor2.style.width = "300px";
+        cursor1.style.transitionDuration = "1s";
+        cursor2.style.transitionDuration = "1s";
+
+    }
+
+    // else if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    //     cursor1.style.width = "100px";
+    //     cursor2.style.width = "100px";
+    // }
+
+    else {
+        cursor1.style.width = "100px";
+        cursor2.style.width = "100px";
+    }
+
+
 }
 
 function featureslideFunction() {
@@ -46,6 +67,7 @@ function individualElement(name) {
         cursorInverted.style.opacity = "1.0";
 
 
+
     }
     else {
         document.body.style.backgroundColor = "#5449E8";
@@ -62,8 +84,8 @@ function aboutScroll() {
     cursorNormal = document.getElementsByClassName('cursor1')[0];
     cursorInverted = document.getElementsByClassName('cursor3')[0];
 
-    aboutNormal = document.getElementsByClassName('imgAbout')[1];
-    aboutInverted = document.getElementsByClassName('imgAbout')[0];
+    aboutNormal = document.getElementsByClassName('imgAbout')[2];
+    aboutInverted = document.getElementsByClassName('imgAbout')[1];
     slidelement = document.getElementsByClassName('about')[0];
     hero = document.getElementsByClassName('hero')[0];
     height = slidelement.offsetHeight;
@@ -85,7 +107,7 @@ function aboutScroll() {
     }
     else {
         aboutNormal.style.opacity = "1.0";
-        aboutInverted.style.opacity = "0.0";
+        aboutInverted.style.opacity = "0.4";
         document.body.style.backgroundColor = "#5449E8";
 
         cursorNormal.style.opacity = "1.0";
